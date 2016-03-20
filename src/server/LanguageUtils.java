496cdc1a8fac16af73a668fca9614f975f30a465
jimgao <admin@jimgao.me>
2016-03-20 00:05:39 -0400

    bye

--- src/server/LanguageUtils.java
+++ src/server/LanguageUtils.java
@@ -10,42 +10,4 @@ public class LanguageUtils {
 
         return null;
     }
-
-    public static boolean compileSourceFile(String workingDirectory, String problemCode, String language){
-        try {
-            if (language.equals("cpp")) {
-                String cmd = "g++ -o " + workingDirectory + problemCode + " " + workingDirectory + language + ".cpp";
-                Process p = Runtime.getRuntime().exec(cmd);
-                int returnCode = p.waitFor();
-
-                if (returnCode == 0)
-                    return true;
-                return false;
-            } else if (language.equals("cpp11")){
-                String cmd = "g++ -o -std=c++11" + workingDirectory + problemCode + " " + workingDirectory +
-                        problemCode + ".cpp";
-
-                Process p = Runtime.getRuntime().exec(cmd);
-                int returnCode = p.waitFor();
-
-                if (returnCode == 0)
-                    return true;
-                return false;
-            } else if (language.equals("java")){
-                String cmd = "javac -d " + workingDirectory + " " + problemCode + ".java";
-                Process p = Runtime.getRuntime().exec(cmd);
-
-                int returnCode = p.waitFor();
-
-                if (returnCode == 0)
-                    return true;
-                return false;
-            }
-
-            return false;
-        } catch (Exception ex){
-            System.err.println("Compilation Error: " + ex.getLocalizedMessage());
-            return false;
-        }
-    }
 }
